ffa10c57b08a66cff58946050bf86de4c8d6d0ee
skersas <ignaxtas@gmail.com>
2024-10-09 23:06:46 +0300

    - Added `get_quantity` method - Added `update_quantity` method - Added `__eq__` method

--- knygis/books.py
+++ knygis/books.py
@@ -9,7 +9,25 @@ class Book:
 
     def __str__(self):
         return f'{self.name}, {self.author}, {self.year}m., {self.genre}, {self.quantity}vnt.'
+    
+    def __eq__(self, other): # __eq__ defines how class instances can be compared
+        if isinstance(other, Book): # is it an instance of Book class
+            return (self.name == other.name and
+                    self.author == other.author and
+                    self.year == other.year and
+                    self.genre == other.genre)
+                    #self.quantity == other.quantity) # do not check for quantity
+        return False # if other is not a Book
+    
+    def get_quantity(self):
+        return self.quantity
+    
+    def update_quantity(self,new_quantity):
+        self.quantity = new_quantity
 
 if __name__ == "__main__":
-    knyga1 = Book("Svetimas","Alberas Kamiu",1942,"romanas")
-    print(knyga1)
+    knyga1 = Book("Svetimas","Alberas Kamiu",1942,"romanas",12)
+    print(knyga1.get_quantity())
+    knyga1.update_quantity(24)
+    print(knyga1.get_quantity())
+    # print(knyga1)
