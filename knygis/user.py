c15431cb54f5b71800e02e4e892ac4ca5e726fa2
skersas <ignaxtas@gmail.com>
2024-10-10 11:03:09 +0300

    created initial class `User` and subclasses `Reader`, `Librarian`

---
+++ knygis/user.py
@@ -0,0 +1,11 @@
+class User:
+    def __init__(self,name):
+        self.name = name
+
+class Reader(User):
+    def __init___(self,name):
+        super().__init__(name)
+
+class Librarian(User):
+    def __init__(self,name):
+        super().__init__(name)
